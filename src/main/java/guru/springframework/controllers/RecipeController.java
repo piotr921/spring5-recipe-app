1c7552954f268d1388eddf6526f2819f9e244507
Piotr Sekula <piotr92345@o2.pl>
2018-09-11 19:12:37 +0200

    Add detailed error message

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -56,10 +56,11 @@ public class RecipeController {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFoundException(){
-        log.error("Handling not found exception");
+    public ModelAndView handleNotFoundException(Exception exception){
+        log.error("Handling not found exception  with message" + exception.getMessage());
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
         return modelAndView;
     }
 }
